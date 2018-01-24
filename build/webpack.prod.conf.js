40592f7ee5aa31bc4d59078b050b8906e924ddb7
ZhangYan <zhangyan@highwho.com>
2018-01-24 19:33:47 +0800

    push

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -27,8 +27,8 @@ var webpackConfig = merge(baseWebpackConfig, {
    output: {
     path: config.bundle.assetsRoot,
     publicPath: config.bundle.assetsPublicPath,
-    filename: 'vue-ruler.min.js',
-    library: 'vue-ruler',
+    filename: 'ruler.min.js',
+    library: 'ruler',
     libraryTarget: 'umd'
   },
   plugins: [
@@ -45,7 +45,7 @@ var webpackConfig = merge(baseWebpackConfig, {
     // extract css into its own file
     new ExtractTextPlugin({
       // filename: utils.assetsPath('css/[name].[contenthash].css')
-       filename: 'vue-ruler.min.css'
+       filename: 'ruler.min.css'
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
